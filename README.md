ac8c57b304e6b2239b1c57b57fc99052ead3d8af
DanPortwine <dan.portwine@protonmail.com>
2020-04-16 14:51:17 +0100

    Setup Laravel, Docker and email verification

---
+++ README.md
@@ -0,0 +1,1 @@
+A place for Traceurs to share spots and challenges.
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
