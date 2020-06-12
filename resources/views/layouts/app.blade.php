dcbb2be0b70de1b70808dc3fb26dc4d9e865800a
DanPortwine <dan.portwine@protonmail.com>
2020-06-12 17:38:32 +0100

    Download font-awesome css file instead of CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet">
 
     <!-- Favicon -->
     <link rel="icon" href="/favicon.png" type="image/png">
