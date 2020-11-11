bbfd20dc7f635be69a8afcf3a36d91d1c8d1f059
DanPortwine <dan.portwine@protonmail.com>
2020-11-11 21:24:56 +0000

    Require unique usernames

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['nullable', 'string', 'max:25'],
+            'name' => ['nullable', 'string', 'max:25', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
