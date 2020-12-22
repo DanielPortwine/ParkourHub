2a1468eb90bc3c79d3ca1f7a76872af6e2dbb409
DanPortwine <dan.portwine@protonmail.com>
2020-12-22 21:50:02 +0000

    Update error page layout file

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -3,11 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="@yield('description', 'Parkour Hub is a place for people to share their spots, challenges, moves and more. There is also a premium membership that boasts a full training system to help users develop their skills and achieve their goals.')">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@stack('title'){{ config('app.name') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/lazyload@2.0.0-rc.2/lazyload.js"></script>
+    @stack('scripts')
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -16,28 +22,41 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/014efa2ea8.js" crossorigin="anonymous"></script>
 
     <!-- Favicon -->
     <link rel="icon" href="/favicon.png" type="image/png">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar sticky-top navbar-dark text-white bg-grey shadow-sm">
-            <div class="container">
-                <a class="navbar-brand sedgwick" href="{{ url('/') }}">
-                    {{ config('app.name') }}
-                </a>
+<div id="app">
+    <nav class="navbar sticky-top navbar-expand-md navbar-dark text-white bg-grey shadow-sm">
+        <div class="container">
+            <a class="navbar-brand sedgwick" href="{{ url('/') }}">
+                {{ config('app.name') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('spots') }}"><i class="fa fa-map nav-icon"></i>Map</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('spot_listing') }}"><i class="fa fa-map-marker nav-icon"></i>Spots</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('challenge_listing') }}"><i class="fa fa-bullseye nav-icon"></i>Challenges</a>
+                    </li>
                 </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main>
-            @yield('content')
-        </main>
-    </div>
+    <main>
+        @yield('content')
+    </main>
+</div>
 </body>
+</html>
