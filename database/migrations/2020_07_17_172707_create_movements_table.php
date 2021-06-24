236f92176a7f72340cd4f0ab6b20f2a144b8210c
DanPortwine <dan.portwine@protonmail.com>
2021-06-24 17:45:11 +0100

    Change description field type to text

--- database/migrations/2020_07_17_172707_create_movements_table.php
+++ database/migrations/2020_07_17_172707_create_movements_table.php
@@ -20,7 +20,7 @@ class CreateMovementsTable extends Migration
             $table->unsignedBigInteger('creator_id')->nullable();
             $table->unsignedBigInteger('type_id');
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->enum('visibility', ['private', 'follower', 'public'])->default('private');
             $table->string('youtube')->nullable();
             $table->integer('youtube_start')->nullable();
